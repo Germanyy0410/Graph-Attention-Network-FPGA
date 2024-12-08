3414100c54226d99fbbbad97926b44abeb975f02
Germanyy0410 <tienduchoang136@gmail.com>
2024-12-08 17:39:48 +0700

    feat [08/12]: update mult

--- rtl/others/mult/mult_8_bit.sv
+++ rtl/others/mult/mult_8_bit.sv
@@ -9,38 +9,42 @@ module mult_8_bit #(
   output signed [OUT_DATA_WIDTH-1:0]  p_o
 );
 
-  logic [IN_DATA_WIDTH-1:0]   a_unsigned;
-  logic [IN_DATA_WIDTH-1:0]   b_unsigned;
-  logic [OUT_DATA_WIDTH-1:0]  p_unsigned;
+  logic [IN_DATA_WIDTH-1:0]   a_abs;
+  logic [IN_DATA_WIDTH-1:0]   b_abs;
+  logic [OUT_DATA_WIDTH-1:0]  p_abs;
 
   generate
     if (MULT_MODE == 0) begin : OPTIMIZED
-      assign p_o        = (~(b_i[7] ^ a_i[7])) ? p_unsigned : (~p_unsigned + 1);
-      assign a_unsigned = a_i[7] ? (~(a_i - 1)) : a_i;
-      assign b_unsigned = b_i[7] ? (~(b_i - 1)) : b_i;
-      assign p_unsigned = (b_unsigned[6] ? {a_unsigned, 6'b0} : 0) +
-                          (b_unsigned[5] ? {a_unsigned, 5'b0} : 0) +
-                          (b_unsigned[4] ? {a_unsigned, 4'b0} : 0) +
-                          (b_unsigned[3] ? {a_unsigned, 3'b0} : 0) +
-                          (b_unsigned[2] ? {a_unsigned, 2'b0} : 0) +
-                          (b_unsigned[1] ? {a_unsigned, 1'b0} : 0) +
-                          (b_unsigned[0] ? {a_unsigned, 0'b0} : 0);
+      assign p_o    = (~(b_i[7] ^ a_i[7])) ? p_abs : (~p_abs + 1);
+
+      assign a_abs  = a_i[7] ? (~(a_i - 1)) : a_i;
+      assign b_abs  = b_i[7] ? (~(b_i - 1)) : b_i;
+      assign p_abs  = (b_abs[6] ? {a_abs, 6'b0} : 0) +
+                      (b_abs[5] ? {a_abs, 5'b0} : 0) +
+                      (b_abs[4] ? {a_abs, 4'b0} : 0) +
+                      (b_abs[3] ? {a_abs, 3'b0} : 0) +
+                      (b_abs[2] ? {a_abs, 2'b0} : 0) +
+                      (b_abs[1] ? {a_abs, 1'b0} : 0) +
+                      (b_abs[0] ? {a_abs, 0'b0} : 0);
     end
     else if (MULT_MODE == 1) begin : ONE_STAGE_OPTIMIZED
       logic signed  [OUT_DATA_WIDTH:0]    p;
       logic signed  [OUT_DATA_WIDTH:0]    p_reg;
-      assign p_o        = p_reg;
-      assign a_unsigned = a_i[7] ? (~(a_i - 1)) : a_i;
-      assign b_unsigned = b_i[7] ? (~(b_i - 1)) : b_i;
-      assign p_unsigned = (b_unsigned[6] ? {a_unsigned, 6'b0} : 0) +
-                          (b_unsigned[5] ? {a_unsigned, 5'b0} : 0) +
-                          (b_unsigned[4] ? {a_unsigned, 4'b0} : 0) +
-                          (b_unsigned[3] ? {a_unsigned, 3'b0} : 0) +
-                          (b_unsigned[2] ? {a_unsigned, 2'b0} : 0) +
-                          (b_unsigned[1] ? {a_unsigned, 1'b0} : 0) +
-                          (b_unsigned[0] ? {a_unsigned, 0'b0} : 0);
-      assign p = ((b_i[7] ^ a_i[7])) ? (~p_unsigned + 1) : p_unsigned;
-      always_ff @(posedge clk or negedge rst_n) begin
+
+      assign p_o    = p_reg;
+      assign p      = ((b_i[7] ^ a_i[7])) ? (~p_abs + 1) : p_abs;
+
+      assign a_abs  = a_i[7] ? (~(a_i - 1)) : a_i;
+      assign b_abs  = b_i[7] ? (~(b_i - 1)) : b_i;
+      assign p_abs  = (b_abs[6] ? {a_abs, 6'b0} : 0) +
+                      (b_abs[5] ? {a_abs, 5'b0} : 0) +
+                      (b_abs[4] ? {a_abs, 4'b0} : 0) +
+                      (b_abs[3] ? {a_abs, 3'b0} : 0) +
+                      (b_abs[2] ? {a_abs, 2'b0} : 0) +
+                      (b_abs[1] ? {a_abs, 1'b0} : 0) +
+                      (b_abs[0] ? {a_abs, 0'b0} : 0);
+
+      always_ff @(posedge clk) begin
         p_reg <= p;
       end
     end
@@ -53,7 +57,8 @@ module mult_8_bit #(
 
       assign p_o = p_reg;
       assign p   = $signed(a_i) * $signed(b_i);
-      always_ff @(posedge clk or negedge rst_n) begin
+
+      always_ff @(posedge clk) begin
         p_reg <= p;
       end
     end
